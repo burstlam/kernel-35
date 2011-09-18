4c882013c34b84c074b6103516c56d200e729338
burstlam <burstlam@gmail.com>
2011-09-18 17:20:21 +0800

    add BFS 404

--- init/main.c
+++ init/main.c
@@ -832,8 +832,8 @@ static noinline int init_post(void)
 	unlock_kernel();
 	mark_rodata_ro();
 	system_state = SYSTEM_RUNNING;
-	numa_default_policy();
-
+	
+	print_scheduler_version();
 
 	current->signal->flags |= SIGNAL_UNKILLABLE;
 
