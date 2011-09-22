7dcd001ba80292e3b1162ebbfdf52806085ad8d6
burst lam <burstlam@gmail.com>
2011-09-23 00:49:23 +0800

    USB: f_mass_storage: Fix sending of CSW if the WRITE command fails
    With CSW_HACK feature, CSW is returned immediately for WRITE commands
    without waiting for data to be written to the media. There are some
    scenarios where because of some error while processing of WRITE command
    (e.g unmounting of media during data transfer), CSW is not sent.
    Handle all such corner cases.
    
    Change-Id: Id795b7ec7afc10e67a3309e6b650dfc5b7867c9e
    CRs-fixed: 281801

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -317,6 +317,10 @@ int os_switch_is_enable(void);
 
 static const char fsg_string_interface[] = "Mass Storage";
 
+#ifdef CONFIG_USB_CSW_HACK
+static int write_error_after_csw_sent;
+static int csw_hack_sent;
+#endif
 
 #define FSG_NO_INTR_EP 1
 #define FSG_NO_DEVICE_STRINGS    1
@@ -877,7 +881,6 @@ static int do_write(struct fsg_common *common)
 	int			rc;
 
 #ifdef CONFIG_USB_CSW_HACK
-	int			csw_hack_sent = 0;
 	int			i;
 #endif
 	if (curlun->ro) {
@@ -3128,9 +3131,10 @@ static int fsg_main_thread(void *common_)
 		 * need to skip sending status once again if it is a
 		 * write scsi command.
 		 */
-		if (common->cmnd[0] == SC_WRITE_6  || common->cmnd[0] == SC_WRITE_10
-					|| common->cmnd[0] == SC_WRITE_12)
+		if (csw_hack_sent) {
+			csw_hack_sent = 0;
 			continue;
+		}
 #endif
 		if (send_status(common))
 			continue;
