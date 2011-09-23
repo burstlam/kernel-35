11d3dd338ee004174bd0aa3e48f305978f5a381e
burst lam <burstlam@gmail.com>
2011-09-24 05:55:42 +0800

    Patch from ck

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -60,7 +60,7 @@ static inline long sync_writeback_pages(unsigned long dirtied)
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 65;
+int dirty_background_ratio = 5;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
